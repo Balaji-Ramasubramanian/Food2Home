7e6cb95f1e5b46b67f91f0ec95e5cdece99e3a8e
Balaji <balaji030698@gmail.com>
2018-09-05 07:41:03 +0530

    added disable composer parameter

--- facebookBot/json_templates/persistent_menu.rb
+++ facebookBot/json_templates/persistent_menu.rb
@@ -4,6 +4,7 @@ class MessengerBot
     "persistent_menu": [
       {
         "locale": "default",
+        "composer_input_disabled": false,
         "call_to_actions": [
           {
             "title": "Menu",
